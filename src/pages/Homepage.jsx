e5a47fb0b47fbaed48a44e2a2a098063f3bb1888
ChiefLopes <chieflopesekede@gmail.com>
2023-11-30 15:25:02 +0100

    modified the stylle for home

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,7 +3,8 @@ import styles from "./Homepage.module.css";
 export default function Homepage() {
   return (
     <main className={styles.homepage}>
-      <section>
+          <section>
+              
         <h1>
           You travel the world.
           <br />
