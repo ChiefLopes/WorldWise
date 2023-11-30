ab87fac7703af53485ce06069bb0c58d17c69edf
ChiefLopes <chieflopesekede@gmail.com>
2023-11-30 15:59:21 +0100

    added some modifications and also added the a link to the logo which directs back to home

--- src/Components/PageNav.jsx
+++ src/Components/PageNav.jsx
@@ -1,20 +1,23 @@
-import React from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./PageNav.module.css"
+import Logo from "./Logo"
+
 
 const PageNav = () => {
   return (
     <div>
-      <nav className={styles.nav}>
+          <nav className={styles.nav}>
+              <Logo />
         <ul>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
+
           <li>
             <NavLink to="/pricing">Pricing</NavLink>
           </li>
           <li>
             <NavLink to="/product">Product</NavLink>
+          </li>
+          <li>
+            <NavLink to="/login">Login</NavLink>
           </li>
         </ul>
       </nav>
