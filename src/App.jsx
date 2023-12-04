be1f56b92665720f58616ad6e58ca5ce0165ff42
ChiefLopes <chieflopesekede@gmail.com>
2023-12-04 21:55:19 +0100

    added some modifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 import Homepage from "./pages/Homepage";
 import Product from "./pages/Product";
@@ -48,9 +48,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/app" element={<AppLayout />}>
             <Route
-              index
-              element={<CityList cities={cities} isLoading={isLoading} />}
-            />
+              index element={<Navigate replace to= "cities"/>} />
             <Route
               path="cities"
               element={<CityList cities={cities} isLoading={isLoading} />}
