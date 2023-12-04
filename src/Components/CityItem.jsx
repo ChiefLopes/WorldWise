b5d97875f2b1225c51bcbe4fb1bb13f91284c307
ChiefLopes <chieflopesekede@gmail.com>
2023-12-04 13:00:26 +0100

    useSearchParams
    Used the useParams hook to read and also modify the query strings in the city URL

--- src/Components/CityItem.jsx
+++ src/Components/CityItem.jsx
@@ -1,5 +1,5 @@
-import styles from './CityItem.module.css'
-import { Link } from 'react-router-dom';
+import styles from "./CityItem.module.css";
+import { Link } from "react-router-dom";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -9,12 +9,14 @@ const formatDate = (date) =>
     weekday: "long",
   }).format(new Date(date));
 
-const CityItem = ({city}) => {
-  const {cityName, emoji, date, id} = city
+const CityItem = ({ city }) => {
+    const { cityName, emoji, date, id, position, country} = city;
     
+    console.log(position);
+
   return (
     <li>
-      <Link className={styles.cityItem} to={`${id}`}>
+      <Link className={styles.cityItem} to={`${id}?lat=${position.lat}&lng=${position.lng}&contry=${country}`}>
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>{formatDate(date)}</time>
@@ -22,6 +24,6 @@ const CityItem = ({city}) => {
       </Link>
     </li>
   );
-}
+};
 
-export default CityItem
+export default CityItem;
--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,11 +1,24 @@
-import styles from './Map.module.css'
+import styles from "./Map.module.css";
+import { useSearchParams } from "react-router-dom";
 
 const Map = () => {
-  return (
-      <div className={styles.mapContainer}>
-          
-    </div>
-  )
-}
+    const [searchParams, setSearchParams] = useSearchParams();
 
-export default Map
+    const lat = searchParams.get("lat");
+    const lng = searchParams.get("lng");
+
+    return (
+      <div className={styles.mapContainer}>
+        <h1>Map</h1>
+        <h1>
+          Position: {lat}, {lng}
+        </h1>
+
+        <button onClick={() => setSearchParams({ lat: 30, lng: 50 })}>
+          Change pos
+        </button>
+      </div>
+    );
+};
+
+export default Map;
