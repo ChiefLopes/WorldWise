f16234e0d4d7bed698ebc5e756e9eba72c7d2da9
ChiefLopes <chieflopesekede@gmail.com>
2023-12-06 19:45:41 +0100

    created a ContextAPI which i used to pass in the state 'isLoading' and 'cities'

--- src/Components/CityList.jsx
+++ src/Components/CityList.jsx
@@ -2,8 +2,12 @@ import CityItem from "./CityItem";
 import styles from "./CityList.module.css";
 import Spinner from "./Spinner";
 import Message from "./Message";
+import { useCities } from "../Contexts/CitiesContext";
 
-const CityList = ({ cities, isLoading }) => {
+const CityList = () => {
+   
+    const { cities, isLoading } = useCities();
+    
     if (isLoading) return <Spinner />;
     
     if (!cities.length) return <Message message={"Add your first city by clicking on a city on the map... "}/>
--- src/Contexts/CitiesContext.jsx
+++ src/Contexts/CitiesContext.jsx
@@ -1,32 +1,12 @@
-import { createContext, useEffect, useState } from "react"
+import { createContext, useEffect, useState, useContext } from "react"
 
 const citiesContext = createContext()
 
 const BASE_URL = "http://localhost:8000";
 
-const CitiesProvider = () => {
-   const [cities, setCities] = useState([]);
-   const [isLoading, setIsLoading] = useState(false);
 
-   useEffect(function () {
-     async function fetchCities() {
-       try {
-         setIsLoading(true);
-         const res = await fetch(`${BASE_URL}/cities`);
-         const data = await res.json();
-         setCities(data);
-         setIsLoading(false);
-       } catch (error) {
-         alert("An error occurred while loading data...");
-       } finally {
-         setIsLoading(false);
-       }import { createContext, useEffect, useState } from "react"
 
-const citiesContext = createContext()
-
-const BASE_URL = "http://localhost:8000";
-
-const CitiesProvider = () => {
+const CitiesProvider = ({ children }) => {
    const [cities, setCities] = useState([]);
    const [isLoading, setIsLoading] = useState(false);
 
@@ -54,20 +34,12 @@ const CitiesProvider = () => {
     )
 }
 
-export default CityContext 
-     }
-     fetchCities();
-   }, []);
-    
-    return (
-        <citiesContext.Provider value={{ cities, isLoading }}>
-            {children}
-        </citiesContext.Provider>
-    )
-}
-
-function useCities () {
+function useCities() {
+    if (useContext === undefined) {
+       throw new Error ('useCities must be used within a CitiesProvider')
+    }
      return useContext(citiesContext)
 }
+    
 
-export {CitiesProvider, } 
+export {CitiesProvider, useCities} 
