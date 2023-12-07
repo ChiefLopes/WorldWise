9a58e978c27827dc3b0010c80d332e330ee0f5b7
Louis Peace <chieflopesekede@gmail.com>
2023-12-07 06:29:27 +0100

    added the BACK button below the city component so that when city is being clicked, there can be a button to redirect back to the previous page

--- src/Components/City.jsx
+++ src/Components/City.jsx
@@ -3,8 +3,8 @@ import styles from "./City.module.css";
 import { useEffect } from "react";
 import { useCities } from "../Contexts/CitiesContext";
 import Spinner from "./Spinner";
-
-
+import { useNavigate } from "react-router-dom";
+import Button from "./Button";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -16,7 +16,8 @@ const formatDate = (date) =>
 
 function City() {
     const { id } = useParams();
-    const {getCity, currentCity, isLoading} = useCities();
+    const { getCity, currentCity, isLoading } = useCities();
+    const move = useNavigate();
     
    
   useEffect(function () {
@@ -58,8 +59,15 @@ function City() {
           Check out {cityName} on Wikipedia &rarr;
         </a>
       </div>
-
-     
+      <Button
+        type="back"
+        onClick={(e) => {
+          //   Used the e.preventDefault function to stop the form action on the button so that the page won't auto-relaod when clicked
+          e.preventDefault();
+          move(-1);
+        }}>
+        &larr; Back
+      </Button>
     </div>
   );
 }
