59a03db13e0f1e3c988572eac53623a86133f36a
Louis Peace <chieflopesekede@gmail.com>
2023-12-07 06:23:09 +0100

    added a feature so that if the current city matches the id, then a border should be placed on the last city clicked

--- src/Components/CityItem.jsx
+++ src/Components/CityItem.jsx
@@ -1,3 +1,4 @@
+import { useCities } from "../Contexts/CitiesContext";
 import styles from "./CityItem.module.css";
 import { Link } from "react-router-dom";
 
@@ -10,13 +11,14 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 const CityItem = ({ city }) => {
+    const {currentCity} = useCities()
+    
     const { cityName, emoji, date, id, position, country} = city;
     
-    console.log(position);
 
   return (
     <li>
-      <Link className={styles.cityItem} to={`${id}?lat=${position.lat}&lng=${position.lng}&contry=${country}`}>
+      <Link className={`${styles.cityItem} ${id === currentCity.id ? styles['cityItem--active'] : ""}`} to={`${id}?lat=${position.lat}&lng=${position.lng}&contry=${country}`}>
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>{formatDate(date)}</time>
