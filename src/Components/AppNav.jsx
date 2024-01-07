9066710c9acc17f71aaad2654a5c3b14ecf84459
Louis Peace <chieflopesekede@gmail.com>
2024-01-07 20:16:20 +0100

    Modified the AppNav

--- src/Components/AppNav.jsx
+++ src/Components/AppNav.jsx
@@ -4,21 +4,23 @@ import { NavLink } from "react-router-dom";
 
 
 const AppNav = () => {
-    return <nav className={styles.nav}>
+    return (
+      <nav className={styles.nav}>
         <ul>
-            <li>
-                {/* Leads to the city route */}
-                <NavLink to="cities">Cities</NavLink>
-            </li>
-            <li>
-                <NavLink to="countries">Countries</NavLink>
-            </li>
-            {/* <li>
+          <li>
+            {/* Leads to the city route */}
+            <NavLink to="cities">Cities</NavLink>
+          </li>
+          <li>
+            {/* Leads to the country route */}
+            <NavLink to="countries">Countries</NavLink>
+          </li>
+          {/* <li>
                 <NavLink to="form">Form</NavLink>
             </li> */}
-       </ul>
-        
-  </nav>;
+        </ul>
+      </nav>
+    );
 };
 
 export default AppNav;
